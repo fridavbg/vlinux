77ed6131063b1daa1254bbb32fed460762f96633
fridavbg <fperssontech@gmail.com>
2022-09-07 11:23:19 +0200

    commands.bash uptime

--- kmom02/script/commands.bash
+++ kmom02/script/commands.bash
@@ -8,49 +8,41 @@
 #  1 on failure
 #
 
-
-
 # Name of the script
-SCRIPT=$( basename "$0" )
+SCRIPT=$(basename "$0")
 
 # Current version
 VERSION="1.0.0"
 
-
-
 #
 # Message to display for usage and help.
 #
-function usage
-{
+function usage {
     local txt=(
-"Utility $SCRIPT for doing stuff."
-"Usage: $SCRIPT [options] <command> [arguments]"
-""
-"Command:"
-"  command1             Demo of command."
-"  command2 [anything]  Demo of command using arguments."
-"  calendar [events]    Print out current calendar with(out) events."
-""
-"Options:"
-"  --help, -h     Print help."
-"  --version, -h  Print version."
+        "Utility $SCRIPT for doing stuff."
+        "Usage: $SCRIPT [options] <command> [arguments]"
+        ""
+        "Command:"
+        "  cal [events]    Print out current calendar with(out) events."
+        "  uptime          Show systems built-in uptime."
+        "  command2 [anything]  Demo of command using arguments."
+        ""
+        "Options:"
+        "  --help, -h     Print help."
+        "  --version, -h  Print version."
     )
 
     printf "%s\\n" "${txt[@]}"
 }
 
-
-
 #
 # Message to display when bad usage.
 #
-function badUsage
-{
+function badUsage {
     local message="$1"
     local txt=(
-"For an overview of the command, execute:"
-"$SCRIPT --help, -h"
+        "For an overview of the command, execute:"
+        "$SCRIPT --help, -h"
     )
 
     [[ -n $message ]] && printf "%s\\n" "$message"
@@ -58,53 +50,44 @@ function badUsage
     printf "%s\\n" "${txt[@]}"
 }
 
-
-
 #
 # Message to display for version.
 #
-function version
-{
+function version {
     local txt=(
-"$SCRIPT version $VERSION"
+        "$SCRIPT version $VERSION"
     )
 
     printf "%s\\n" "${txt[@]}"
 }
 
-
-
 #
 # Function for taking care of specific command. Name the function as the
 # command is named.
 #
-function app-command1
-{
-    echo "This is output from command1."
+function app-uptime {
+    echo "This is output from command3, showing the system uptime."
+    
+        echo
+        uptime
 }
 
-
-
 #
 # Function for taking care of specific command. Name the function as the
 # command is named.
 #
-function app-command2
-{
+function app-command2 {
     echo "This is output from command2."
     echo "Command 2 takes additional arguments which currently are:"
     echo " Number of arguments = '$#'"
     echo " List of arguments = '$*'"
 }
 
-
-
 #
 # Function for taking care of specific command. Name the function as the
 # command is named.
 #
-function app-cal
-{
+function app-cal {
     local events="$1"
 
     echo "This is output from command3, showing the current calender."
@@ -116,37 +99,34 @@ function app-cal
     fi
 }
 
-
-
 #
 # Process options
 #
-while (( $# ))
-do
+while (($#)); do
     case "$1" in
 
-        --help | -h)
-            usage
-            exit 0
+    --help | -h)
+        usage
+        exit 0
         ;;
 
-        --version | -v)
-            version
-            exit 0
+    --version | -v)
+        version
+        exit 0
         ;;
 
-        command1         \
-        | command2       \
-        | cal)
-            command=$1
-            shift
-            app-"$command" "$*"
-            exit 0
+    cal | \
+        uptime | \
+        command2)
+        command=$1
+        shift
+        app-"$command" "$*"
+        exit 0
         ;;
 
-        *)
-            badUsage "Option/command not recognized."
-            exit 1
+    *)
+        badUsage "Option/command not recognized."
+        exit 1
         ;;
 
     esac
