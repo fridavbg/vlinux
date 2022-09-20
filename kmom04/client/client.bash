0d29327585a8d7a7c9fc0f9ede741ccc07c1fccd
fridavbg <fperssontech@gmail.com>
2022-09-20 19:32:08 +0200

    client.bash help menu

--- kmom04/client/client.bash
+++ kmom04/client/client.bash
@@ -8,49 +8,42 @@
 #  1 on failure
 #
 
-
-
 # Name of the script
-SCRIPT=$( basename "$0" )
+SCRIPT=$(basename "$0")
 
 # Current version
 VERSION="1.0.0"
 
-
-
 #
 # Message to display for usage and help.
 #
-function usage
-{
+function usage {
     local txt=(
-"Utility $SCRIPT for doing stuff."
-"Usage: $SCRIPT [options] <command> [arguments]"
-""
-"Command:"
-"  command1             Demo of command."
-"  command2 [anything]  Demo of command using arguments."
-"  calendar [events]    Print out current calendar with(out) events."
-""
-"Options:"
-"  --help, -h     Print help."
-"  --version, -h  Print version."
+        "Utility $SCRIPT for doing stuff."
+        "Usage: $SCRIPT [options] <command> [arguments]"
+        ""
+        "Command:"
+        "  all             Call route /all."
+        "  names           Call route /names."
+        "  color [color]  Call route /color/:color."
+        "  test [url]    Check if servers is running."
+        ""
+        "Options:"
+        "  --help, -h     Print help."
+        "  --version, -h  Print version."
     )
 
     printf "%s\\n" "${txt[@]}"
 }
 
-
-
 #
 # Message to display when bad usage.
 #
-function badUsage
-{
+function badUsage {
     local message="$1"
     local txt=(
-"For an overview of the command, execute:"
-"$SCRIPT --help"
+        "For an overview of the command, execute:"
+        "$SCRIPT --help"
     )
 
     [[ -n $message ]] && printf "%s\\n" "$message"
@@ -58,31 +51,93 @@ function badUsage
     printf "%s\\n" "${txt[@]}"
 }
 
+#
+# Message to display for version.
+#
+function version {
+    local txt=(
+        "$SCRIPT version $VERSION"
+    )
+
+    printf "%s\\n" "${txt[@]}"
+}
+
+#
+# Message to display for version.
+#
+function save {
+    local txt=(
+        "$SCRIPT version $VERSION"
+    )
+
+    printf "%s\\n" "${txt[@]}"
+}
+
+#
+# Function for taking care of specific command. Name the function as the
+# command is named.
+#
+function app-all {
+    echo "All itemss"
+}
+
+#
+# Function for taking care of specific command. Name the function as the
+# command is named.
+#
+function app-names {
+    echo "All item names"
+}
+
+#
+# Function for taking care of specific command. Name the function as the
+# command is named.
+#
+function app-color {
+    echo "Specific item color"
+}
+
+#
+# Function for taking care of specific command. Name the function as the
+# command is named.
+#
+function app-test {
+    echo "Test url"
+}
 
 #
 # Process options
 #
-while (( $# ))
-do
+while (($#)); do
     case "$1" in
 
-        --help | -h)
-            usage
-            exit 0
+    --help | -h)
+        usage
+        exit 0
         ;;
 
-        # command1         \
-        # | command2       \
-        # | calendar)
-        #     command=$1
-        #     shift
-        #     app-"$command" "$*"
-        #     exit 0
-        # ;;
+    --version | -v)
+        version
+        exit 0
+        ;;
 
-        *)
-            badUsage "Option/command not recognized."
-            exit 1
+    --save | -s)
+        save
+        exit 0
+        ;;
+
+    all | \
+        names | \
+        calendar)
+        command=$1
+        shift
+        app-"$command" "$*"
+        exit 0
+        ;;
+
+    *)
+        badUsage "Option/command not recognized."
+        exit 1
         ;;
 
     esac
