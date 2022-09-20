9657b84c5b003cdc451be28094a043a1dd8b2db5
fridavbg <fperssontech@gmail.com>
2022-09-20 15:20:02 +0200

    dockerhub.bash chanegs

--- kmom03/dockerhub.bash
+++ kmom03/dockerhub.bash
@@ -1,2 +1,3 @@
-echo 
-"docker run -it -p 8080:80 -v $(pwd)/mysite.vlinux.se:/var/www/vhosts/mysite.vlinux.se --add-host mysite.vlinux.se:127.0.0.1 fridavbg/vlinux-vhost:1.0"
+#!/bin/bash
+
+docker run -p 8080:80 --name mysite -v "$(pwd)/$1":/var/www/vhosts/mysite.vlinux.se --add-host mysite.vlinux.se:127.0.0.1 fridavbg/vlinux-vhost:1.0
