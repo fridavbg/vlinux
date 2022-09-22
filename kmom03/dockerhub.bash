0d562e3c57d95e76485e65e580e65434d5fec486
fridavbg <fperssontech@gmail.com>
2022-09-22 14:15:34 +0200

    Fixed kmom03

--- kmom03/dockerhub.bash
+++ kmom03/dockerhub.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8080:80 --name mysite -v "$(pwd)/$1":/var/www/vhosts/mysite.vlinux.se --add-host mysite.vlinux.se:127.0.0.1 fridavbg/vlinux-vhost:1.0
+docker run -p 8080:80 -d --name mysite -v "$(pwd)/$1":/var/www/vhosts/mysite.vlinux.se --add-host mysite.vlinux.se:127.0.0.1 fridavbg/vlinux-vhost:1.0
