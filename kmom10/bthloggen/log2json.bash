00d369e3255623d25f4d5436a0e8b9f05d7bd5f9
fridavbg <fperssontech@gmail.com>
2022-10-12 17:20:28 +0200

    url regex

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# ip_adresses_regex = '/([0-9]{1,3}\.){3}[0-9]{1,3}/p'
-# url_regex = '/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)/p'
+# ip_adresses_regex = '/(([0-9]{1,3}\.){3}([0-9]{1,3}))/p'
+# url_regex = '/[A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+/p'
 
 # sed -n -E '(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)' <access-50k.log
 
