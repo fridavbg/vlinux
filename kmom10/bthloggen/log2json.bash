d4818a589c2f209148e4799a90a3842fb3541c7d
fridavbg <fperssontech@gmail.com>
2022-10-14 18:57:28 +0200

    ip-adresses

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -3,13 +3,11 @@
 file='access-50k.log'
 
 regex_ip='([0-9]{1,3}\.){3}([0-9]{1,3})'
-regex_url='([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)'
-
-# regex_ip_url = (([0-9]{1,3}\.){3}([0-9]{1,3}))||([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)
-
-# sed -n '/'$regex_ip_url'/p' | cut -d ' ' -f 1 <access-50k.log
+regex_url='(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+))'
 
 # grep $regex_ip_url $file | awk -F' ' '{print { ip: $1, url: $2}}'
 
-grep -oP $regex_url $file
+# sed -rn 's/'$regex_ip'.*'$regex_url'/{"ip": "\1","url": "\2"}/p'
+
+grep -oP $regex_ip.*$regex_url.* $file | awk -F' ' '{print $1}' 
 
