c0d1b7245ca43d9317e77da20497420060bfcda6
fridavbg <fperssontech@gmail.com>
2022-10-14 17:42:33 +0200

    regex & file variables

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-regex_ip=(([0-9]{1,3}\.){3}([0-9]{1,3}))
-regex_url=([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)
-regex_ip_url = (([0-9]{1,3}\.){3}([0-9]{1,3}))||([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)
 
-sed -n -E '/([0-9]{1,3}\.){3}[0-9]{1,3}/p' | cut -d ' ' -f 1 <access-50k.log
+file='access-50k.log'
+
+regex_ip='([0-9]{1,3}\.){3}([0-9]{1,3})'
+regex_url='([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)'
+
+# regex_ip_url = (([0-9]{1,3}\.){3}([0-9]{1,3}))||([A-Za-z]+:\/\/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_:%&;\?\#\/.=]+)
+
+# sed -n '/'$regex_ip_url'/p' | cut -d ' ' -f 1 <access-50k.log
+
+# grep $regex_ip_url $file | awk -F' ' '{print { ip: $1, url: $2}}'
+
+grep -oP $regex_url $file
+
