fb6a1bbfc84a49658a27dc8c76b56ff6b5a31a3e
fridavbg <fperssontech@gmail.com>
2022-10-14 19:38:00 +0200

    nära skjuter ingen ip hare

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 file='access-50k.log'
+test_log='test.log'
 
 regex_ip='([0-9]{1,3}\.){3}([0-9]{1,3})'
 regex_url='(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+))'
 
 # grep $regex_ip_url $file | awk -F' ' '{print { ip: $1, url: $2}}'
 
-# sed -rn 's/'$regex_ip'.*'$regex_url'/{"ip": "\1","url": "\2"}/p'
+sed -rn 's/.*([0-9]{1,3}\.){3}([0-9]{1,3}).*(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)).*/{\n"ip": "\1\2",\n"url": "\3" \n}/p' $test_log
 
-grep -oP $regex_ip.*$regex_url.* $file | awk -F' ' '{print $1}' 
-
+# grep -oP $regex_ip.*$regex_url.* $file | awk -F' ' '{print $1}' 
