b1fdafb9aa9ce734e2ef31e6a8a0772d8e58e766
fridavbg <fperssontech@gmail.com>
2022-10-14 19:53:46 +0200

    regex print out

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -8,6 +8,6 @@ regex_url='(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+))'
 
 # grep $regex_ip_url $file | awk -F' ' '{print { ip: $1, url: $2}}'
 
-sed -rn 's/.*([0-9]{1,3}\.){3}([0-9]{1,3}).*(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)).*/{\n"ip": "\1\2",\n"url": "\3" \n}/p' $test_log
+sed -rn 's/(([0-9]{1,3}\.){3}([0-9]{1,3})).*(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)).*/{\n"ip": "\1",\n"url": "\4" \n}/p' $test_log
 
 # grep -oP $regex_ip.*$regex_url.* $file | awk -F' ' '{print $1}' 
