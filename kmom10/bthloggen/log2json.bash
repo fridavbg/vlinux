0ccac69d9497821657d006104e1f072e8af785f9
fridavbg <fperssontech@gmail.com>
2022-10-15 14:38:24 +0200

    log.json generated in valid json.format

--- kmom10/bthloggen/log2json.bash
+++ kmom10/bthloggen/log2json.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-file='access-50k.log'
-test_log='test.log'
+log_file='access-50k.log'
+log_test='test.log'
+json_file='./data/log.json'
 
 regex_ip='([0-9]{1,3}\.){3}([0-9]{1,3})'
 regex_url='(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+))'
 
-# grep $regex_ip_url $file | awk -F' ' '{print { ip: $1, url: $2}}'
+sed -rn 's/(([0-9]{1,3}\.){3}([0-9]{1,3})).*(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)).*/{\n"ip": "\1",\n"url": "\4" \n},/w ./data/log.json' $log_test
 
-sed -rn 's/(([0-9]{1,3}\.){3}([0-9]{1,3})).*(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+)).*/{\n"ip": "\1",\n"url": "\4" \n}/p' $test_log
+sed -i '1s/^/[\n/' $json_file
 
-# grep -oP $regex_ip.*$regex_url.* $file | awk -F' ' '{print $1}' 
+sed -i '$ s/.$/\]/' $json_file
