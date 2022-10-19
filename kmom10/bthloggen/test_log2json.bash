d7bfc3e1ecbf433c6e928dea4aef409606c977cd
fridavbg <fperssontech@gmail.com>
2022-10-19 13:03:20 +0200

    mer data json file!

--- kmom10/bthloggen/test_log2json.bash
+++ kmom10/bthloggen/test_log2json.bash
@@ -7,15 +7,7 @@ regex_ip='([0-9]{1,3}\.){3}([0-9]{1,3})'
 
 regex_url='(https?:\/\/(www\.)?([a-zA-Z0-9]+)(\.[a-zA-Z0-9.-]+))'
 
-regex_day='(?<=\[)[0-3][1-9](?=\/)'
-# works with
-# sed 's/(?<=\[)[0-3][1-9](?=\/)//p' test.log
-
-regex_month='(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
-
-regex_time='((?<=:)[0-9][0-9]:[0-9][0-9]:[0-9][0-9])'
-# works with
-# sed 's/((?<=:)[0-9][0-9]:[0-9][0-9]:[0-9][0-9])//p' test.log
+regex_date='[0-3][1-9]).(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).([0-9]{4}).([0-9][0-9]:[0-9][0-9]:[0-9][0-9]'
 
 if [[ -f $json_test ]]; then
     echo "File exits"
@@ -24,10 +16,13 @@ if [[ -f $json_test ]]; then
 fi
 
 # Extract lines with both ip & urls and print in JSON format
-sed -rn 's/('"$regex_ip"').*('"$regex_month"').*('"$regex_url"').*/{\n"ip": "\1",\n"day": "\2",\n"month": "\4",\n"time": "Time",\n"url": "\6"\n},/w '"$json_test"'' $log_test
+# sed -rn 's/('"$regex_ip"').*('"$regex_month"').*('"$regex_url"').*/{\n"ip": "\1",\n"day": "Day",\n"month": "\4",\n"time": "Time",\n"url": "\6"\n},/w '"$json_test"'' $log_test
+
+# DAY DEBUG
+sed -rn 's/.*('"$regex_ip"').*('"$regex_date"').*('"$regex_url"').*/{\n"ip": "\1",\n"day": "\4",\n"month": "\5",\n"time": "\7",\n"url": "\8"\n},/w '"$json_test"'' $log_test
 
 # Add [ to beginning of file
 sed -i '1s/^/[\n/' $json_test
 
 # Replace last , with a ]
-sed -i '$ s/.$/\]/' $json_test
+sed -i '$ s/.$/\n]/' $json_test
