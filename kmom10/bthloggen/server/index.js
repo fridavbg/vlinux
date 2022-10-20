b48c13f09384f9bfdbbc2cbd57439e29fc87ca13
fridavbg <fperssontech@gmail.com>
2022-10-20 14:45:35 +0200

    All server req.query combinations I could think of is working in this one *celebratory dance moves*

--- kmom10/bthloggen/server/index.js
+++ kmom10/bthloggen/server/index.js
@@ -14,12 +14,22 @@ app.get("/data", (req, res) => {
             const result = jsonLog.filter((item) =>
                 item.ip.includes(req.query.ip)
             );
+            if (result.length === 0) {
+                return res.send(
+                    "Nothing was found in the log for ip: " + req.query.ip
+                );
+            }
             return res.json(result);
         }
         if (req.query.day) {
             const result = jsonLog.filter((item) =>
                 item.day.includes(req.query.day)
             );
+            if (result.length === 0) {
+                return res.send(
+                    "Nothing was found in the log for day: " + req.query.day
+                );
+            }
             return res.json(result);
         }
         if (req.query.month) {
@@ -63,6 +73,12 @@ app.get("/data", (req, res) => {
                         return item.time;
                     }
                 });
+                if (result.length === 0) {
+                    return res.send(
+                        "Nothing was found in he log for time: " +
+                            req.query.time
+                    );
+                }
                 return res.json(result);
             } else {
                 let hour = req.query.time;
@@ -79,10 +95,15 @@ app.get("/data", (req, res) => {
             const result = jsonLog.filter((item) =>
                 item.url.includes(req.query.url)
             );
+            if (result.length === 0) {
+                return res.send(
+                    "Nothing was found in he log for url: " + req.query.url
+                );
+            }
             return res.json(result);
         }
         return res.send(
-            "Nothing was found with that property, please search for a url or an ip address"
+            "Nothing was found with that property, please search for: ip, day, month, time or url"
         );
     }
     if (Object.entries(req.query).length === 2) {
@@ -118,6 +139,14 @@ app.get("/data", (req, res) => {
                         return item;
                     }
                 });
+                if (result.length === 0) {
+                    return res.send(
+                        "Nothing was found in he log for that time " +
+                            req.query.time +
+                            " and day " +
+                            req.query.day
+                    );
+                }
                 return res.json(result);
             } else {
                 let hour = req.query.time;
@@ -129,6 +158,14 @@ app.get("/data", (req, res) => {
                         return item;
                     }
                 });
+                if (result.length === 0) {
+                    return res.send(
+                        "Nothing was found in he log for that time " +
+                            req.query.time +
+                            " and day " +
+                            req.query.day
+                    );
+                }
                 return res.json(result);
             }
         }
@@ -171,6 +208,16 @@ app.get("/data", (req, res) => {
                         return item;
                     }
                 });
+                if (result.length === 0) {
+                    return res.send(
+                        "Nothing was found in he the log for that month " +
+                            req.query.month +
+                            " with day " +
+                            req.query.day +
+                            " time " +
+                            req.query.time
+                    );
+                }
                 return res.json(result);
             } else {
                 let hour = req.query.time;
