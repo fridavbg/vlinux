88db9baaa55ac059f91eb6718dc12d2a8df18f4b
fridavbg <fperssontech@gmail.com>
2022-10-22 21:08:24 +0200

    webbclient search options

--- kmom10/bthloggen/webbclient/kmom10.vlinux.se/src/js/index.js
+++ kmom10/bthloggen/webbclient/kmom10.vlinux.se/src/js/index.js
@@ -1,21 +1,28 @@
-function dropdownSelected(value) {
-    const options = [
-        "URL",
-        "IP",
-        "Month",
-        "Day",
-        "Time",
-        "Day-Time",
-        "Month-Day-Time",
-    ];
+const el = document.getElementById("search-options");
 
-    for (const option of options) {
-        if (option === "Day-Time") {
-            console.log(value);
-        }
-        elif (option === "Month-Day-Time") {
-            console.log(value);
-        }
-        console.log(value);
+el.addEventListener("change", function handleChange(value) {
+    let searchForm = document.createElement("form");
+    if (value === "Day-Time") {
+        searchForm.innerHTML =
+            "<label>Day: </label>" +
+            "<input name='day' value='day'/>" +
+            "<label>Time: </label>" +
+            "<input name='time' value='time'/>";
+    } else if (value === "Month-Day-Time") {
+        searchForm.innerHTML =
+            "<label>Month: </label>" +
+            "<input name='Month' value='Month'/>" +
+            "<label>Day: </label>" +
+            "<input name='day' value='day'/>" +
+            "<label>Time: </label>" +
+            "<input name='time' value='time'/>";
     }
-}
+    searchForm.innerHTML = `
+    <label>${value}: </label>
+    <input
+    name='${value}' 
+    value='${value}'
+    />
+    `;
+    console.log(searchForm);
+});
